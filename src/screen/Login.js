cae02b8c0df8aac7b3915783e3b3166569c618ed
feelingBlue00 <pta230500x090800@gmail.com>
2021-10-13 14:57:37 +0700

    New: Add dependencies and packages for react-navigation, react-native-screens, react-native-safe-area-context + modified MainActivity

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-const Login = () => {};
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+});
 
 export default Login;
