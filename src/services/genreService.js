eb3863d5133c661b80825a08274facd41ff43b45
itachi176 <lhoang17062000@gmail.com>
2021-12-06 09:05:48 +0700

    add top views

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -4,7 +4,7 @@ import {baseUrl} from '../constants/url/url';
 
 const getAllGenres = () => {
   const request = axios.get(`${baseUrl}/genre/all`);
-  return request.then(response => response.json());
+  return request.then(response => response.data);
 };
 
 export {getAllGenres};
